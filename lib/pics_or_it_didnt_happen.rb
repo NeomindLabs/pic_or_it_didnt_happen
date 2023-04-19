8692f261cba67f522e304825954261a017bd48d0
Greg Matthew Crossley <greg@crossley.to>
2023-04-19 13:35:33 -0400

    added classes

--- lib/pics_or_it_didnt_happen.rb
+++ lib/pics_or_it_didnt_happen.rb
@@ -1,16 +1,17 @@
 require 'base64'
 
 module PicsOrItDidntHappen
-  def self.image_to_data_url_image_tag(file_path, alt_text: nil)
+  def self.image_to_data_url_image_tag(file_path, alt_text: nil, class: nil)
     raise ArgumentError, "No file was found at #{file_path}" unless File.exist?(file_path)
+    raise ArgumentError, "alt_text must be a string" unless alt_text.is_a?(String) || alt_text.nil?
+    raise ArgumentError, "class must be a string" unless class.is_a?(String) || class.nil?
     file_in_binary = IO.binread(file_path)
     base64_encoded_image_data = [file_in_binary].pack('m0')
-    src_string = "data:#{mime_type_of(file_path)};base64,#{base64_encoded_image_data}"    
-    if alt_text
-      return "<img src=\"#{src_string}\" alt=\"#{alt_text}\">"
-    else
-      return "<img src=\"#{src_string}\">"
-    end
+    src_string = "data:#{mime_type_of(file_path)};base64,#{base64_encoded_image_data}"
+    html_string = "<img src=\"#{src_string}\""
+    html_string += " alt=\"#{alt_text}\"" if alt_text   
+    html_string += " class=\"#{class}\"" if class 
+    return html_string
   end
 
   # this method was inspired by a Alain Beauvois's StackOverflow answer: https://stackoverflow.com/a/16635245
